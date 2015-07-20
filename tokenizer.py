e03cfeec658ded8db1e086e39b2ea8ff1d3c8093
Xiang Yu <eggplant.elf@gmail.com>
2015-07-20 14:21:42 +0200

    improve tokenizer

--- tokenizer.py
+++ tokenizer.py
@@ -5,9 +5,30 @@ from segtok.segmenter import split_multi
 from segtok.tokenizer import word_tokenizer, split_contractions
 from pymongo import MongoClient
 import sys
+import re
 
 
 def tokenize(output_file, db = 'crawler'):
+    client = MongoClient()
+    texts = client[db]['texts']
+    f = open(output_file, 'w')
+    # (TODO: some query to get specific data)
+    for entry in texts.find():
+        text = entry['text'].decode('utf-8', 'ignore')
+        # (optional: write article level data)
+        for sent in split_multi(text):
+            for token in word_tokenizer(sent):
+                if re.search("'s$", token):
+                    f.write('%s\t%s\n' % (token[:-2].encode('utf-8', 'ignore'), 'X'))
+                    f.write('%s\t%s\n' % (token[-2:].encode('utf-8', 'ignore'), 'X'))
+                else:
+                    f.write('%s\t%s\n' % (token.encode('utf-8', 'ignore'), 'X'))
+                    
+            f.write('\n')
+    f.close()
+
+
+def tokenize_old(output_file, db = 'crawler'):
     client = MongoClient()
     texts = client[db]['texts']
     f = open(output_file, 'w')
